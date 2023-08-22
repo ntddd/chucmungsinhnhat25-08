b5fd8db93ed381deaf0883c70f1f14b9fe84bfa2
ntddd <130540768+ntddd@users.noreply.github.com>
2023-08-22 11:22:37 +0700

    Add files via upload
    ssasa

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Gửi Tặng Em';
+const contentCard = 'Hôm nay là một ngày đặc biệt, ngày sinh nhật người con gái rất tuyệt vời (Dù đã có vài điểm không tốt nhưng là xu thế nên chấp nhận được). Chúc em luôn tươi trẻ, khỏe mạnh. Mong rằng những điều may mắn, tốt đẹp nhất sẽ đến với em. Happy Birthday.';
+
+const giftImage = 'ava2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
